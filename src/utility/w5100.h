3cd7ccae19c5a71b9a2c66bd4fd8815443efbff5
ivizot <ivizot@gmail.com>
2021-04-16 14:27:50 +0300

    Update w5100.h

--- src/utility/w5100.h
+++ src/utility/w5100.h
@@ -23,7 +23,7 @@
 #endif
 
 // Safe for all chips
-#define SPI_ETHERNET_SETTINGS SPISettings(14000000, MSBFIRST, SPI_MODE0)
+#define SPI_ETHERNET_SETTINGS SPISettings(SPI_ETHERNET_SPEED, MSBFIRST, SPI_MODE0)
 
 // Safe for W5200 and W5500, but too fast for W5100
 // Uncomment this if you know you'll never need W5100 support.
