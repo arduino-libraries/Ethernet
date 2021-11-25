761102f01fe30f3c227c48d86943019689b35db1
Ben Jones <ben.jones12@gmail.com>
2021-11-26 09:47:01 +1300

    removed one bodge (no longer needed after upgrade to esp v2.0.1)

--- src/Ethernet.h
+++ src/Ethernet.h
@@ -219,18 +219,6 @@ public:
 	uint8_t status();
 	virtual int connect(IPAddress ip, uint16_t port);
 	virtual int connect(const char *host, uint16_t port);
-#ifdef ESP32
-	int connect(IPAddress ip, uint16_t port, int timeout)
-	{
-		connect(ip, port);
-		setConnectionTimeout(timeout);
-	}
-	int connect(const char* host, uint16_t port, int timeout)
-	{
-		connect(host, port);
-		setConnectionTimeout(timeout);
-	}
-#endif
 	virtual int availableForWrite(void);
 	virtual size_t write(uint8_t);
 	virtual size_t write(const uint8_t *buf, size_t size);
