064a37ded464c4fb809daf2643277db19b42e88f
Hans Unzner <hansunzner@gmail.com>
2024-01-10 10:46:40 +0100

    DhcpAddressPrinter example: Fix initialization with no hardware present

--- examples/DhcpAddressPrinter/DhcpAddressPrinter.ino
+++ examples/DhcpAddressPrinter/DhcpAddressPrinter.ino
@@ -42,17 +42,20 @@ void setup() {
 
   // start the Ethernet connection:
   Serial.println("Initialize Ethernet with DHCP:");
+
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    } else if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-    // no point in carrying on, so do nothing forevermore:
+  }
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    // do nothing, no point running without Ethernet hardware
     while (true) {
       delay(1);
     }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
   }
   // print your local IP address:
   Serial.print("My IP address: ");
