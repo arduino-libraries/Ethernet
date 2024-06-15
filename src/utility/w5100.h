d83c1eaef95d2428282c5fec48c2e6abcaa9d658
Joey Parrish <joey.parrish@gmail.com>
2024-06-15 11:29:07 -0700

    Make SPI_ETHERNET_SETTINGS and MAX_SOCK_NUM configurable at the project level
    Closes #267

--- src/utility/w5100.h
+++ src/utility/w5100.h
@@ -18,7 +18,9 @@
 #include <SPI.h>
 
 // Safe for all chips
-#define SPI_ETHERNET_SETTINGS SPISettings(14000000, MSBFIRST, SPI_MODE0)
+#if !defined(SPI_ETHERNET_SETTINGS)
+# define SPI_ETHERNET_SETTINGS SPISettings(14000000, MSBFIRST, SPI_MODE0)
+#endif
 
 // Safe for W5200 and W5500, but too fast for W5100
 // Uncomment this if you know you'll never need W5100 support.
